da3ed9e13deef1aef35954e7a1e1ea9709e912f6
1415181920@qq.com <1415181920@qq.com>
2023-07-25 17:00:15 +0800

    Init Java包扫描机制案例

---
+++ cocoyam-serve/src/main/java/io/xiaoyu/Application.java
@@ -0,0 +1,23 @@
+package io.xiaoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello 1111111111111");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "welcome to cocoyam";
+    }
+
+}
+
