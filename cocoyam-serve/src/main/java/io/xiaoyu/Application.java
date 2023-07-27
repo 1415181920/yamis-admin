91053602f65eee66faeb20fe5f9702ee08c5ec1f
1415181920@qq.com <1415181920@qq.com>
2023-07-27 11:36:24 +0800

    基本构建

--- cocoyam-serve/src/main/java/io/xiaoyu/Application.java
+++ cocoyam-serve/src/main/java/io/xiaoyu/Application.java
@@ -1,5 +1,6 @@
 package io.xiaoyu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@MapperScan(basePackages = {"io.xiaoyu.**.mapper"})
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello 1111111111111");
         SpringApplication.run(Application.class, args);
+
     }
 
     @GetMapping("/")
