07cd0ba468e80a257309fe16d926c8c13367f3c6
cocoyam <1415181920@qq.com>
2023-11-12 23:59:59 +0800

    feat: ✨增加isCreate和isEdit方法判断当前操作属于新增还是编辑

--- cocoyam-common/src/main/java/io/xiaoyu/common/basic/controller/AdminBaseViewController.java
+++ cocoyam-common/src/main/java/io/xiaoyu/common/basic/controller/AdminBaseViewController.java
@@ -1,21 +1,45 @@
 package io.xiaoyu.common.basic.controller;
 
+import io.xiaoyu.common.shared.YamisActionContext;
 import io.xiaoyu.common.yaims.AmisFactory;
 
 import java.util.HashMap;
 
 public class AdminBaseViewController {
 
-    protected  HashMap<Object, Object> DialogButton(HashMap<Object, Object> render,String label,String icon){
-        System.err.println(render);
+    protected  <T extends AdminBaseViewController> HashMap<Object, Object> createButton(T t){
+        YamisActionContext.setYamisActionCreate();
        return AmisFactory.DialogAction().dialog(
-                AmisFactory.Dialog().title(label).body(render).render()
-        ).label(label).icon(icon).render();
+                AmisFactory.Dialog().title("新增").body(t.formView()).render()
+        ).label("新增").icon("fa fa-add").render();
     }
+
+
+
+    protected <T extends AdminBaseViewController>HashMap<Object, Object> rowEditButton(T t) {
+        YamisActionContext.setYamisActionEdit();
+        return AmisFactory.DialogAction().dialog(
+                AmisFactory.Dialog().title("编辑").body(t.formView()).render()
+        ).label("编辑").icon("fa fa-edit").level("link").render();
+    }
+
 
     protected io.xiaoyu.common.yaims.Form baseForm(String api,String title){
        return AmisFactory.Form().panelClassName("px-48 m:px-0").
                 mode("horizontal").api(api).title(title);
+    }
+
+    protected boolean isCreate(){
+       return  YamisActionContext.isYamisActionCreate();
+    }
+
+    protected boolean isEdit(){
+        return YamisActionContext.isYamisActionEdit();
+    }
+
+
+    protected HashMap<Object,Object> formView(){
+        return null;
     }
 
 }
