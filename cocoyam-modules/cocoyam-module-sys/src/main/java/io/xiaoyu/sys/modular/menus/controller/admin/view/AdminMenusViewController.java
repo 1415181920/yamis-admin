0b1265d8bc8f0ea9fcb4a6569a9825bbd7f16b04
cocoyam <1415181920@qq.com>
2023-12-09 13:27:32 +0800

    perf: ⚡️菜单前端页面调整

--- cocoyam-modules/cocoyam-module-sys/src/main/java/io/xiaoyu/sys/modular/menus/controller/admin/view/AdminMenusViewController.java
+++ cocoyam-modules/cocoyam-module-sys/src/main/java/io/xiaoyu/sys/modular/menus/controller/admin/view/AdminMenusViewController.java
@@ -96,6 +96,7 @@ public class AdminMenusViewController extends AdminBaseViewController{
 
         return baseForm(api,title).body(
             new Object[]{
+
                 AmisFactory.GroupControl().body(new Object[]{
                     AmisFactory.Hidden().name("id").label("id").render(),
                     AmisFactory.TextControl().name("title").label("菜单名称").required(true).render(),
@@ -103,16 +104,94 @@ public class AdminMenusViewController extends AdminBaseViewController{
                             description("菜单图标"+"<a href=\"https://icones.js.org/collection/all\" target=\"_blank\"> https://icones.js.org</a>").
                             render(),
                 }).render(),
-                AmisFactory.Hidden().name("id").label("id").render(),
-                AmisFactory.TextControl().name("parentId").label("parentId").render(),
-                AmisFactory.TextControl().name("order").label("排序").render(),
-                AmisFactory.TextControl().name("title").label("菜单名称").render(),
-                AmisFactory.TextControl().name("icon").label("菜单图标").render(),
-                AmisFactory.TextControl().name("url").label("菜单路由").render(),
-                AmisFactory.TextControl().name("urlType").label("路由类型(1:路由,2:外链)").render(),
-                AmisFactory.TextControl().name("visible").label("是否可见").render(),
-                AmisFactory.TextControl().name("isHome").label("是否为首页").render(),
-                AmisFactory.TextControl().name("extension").label("扩展").render(),
+                AmisFactory.GroupControl().body(new Object[]{
+                    AmisFactory.TreeSelectControl().name("parentId").
+                            label("父级菜单").labelField("title").valueField("id").
+                            showIcon(false).value(0).source("/sys/menus/admin-menus/query-list").render(),
+                    AmisFactory.TextControl().name("component").label("组件").description("默认为 amis , 非自定义前端页面请勿修改")
+                            .value("amis").render(),
+                }).render(),
+
+                AmisFactory.TextControl().name("url").label("菜单路由")
+                        .required(true)
+                        .validateOnChange(true)
+                        .validations(new HashMap<String,Object>(){{
+                            put("matchRegexp","/^(http(s)?\\:\\/)?(\\/)+/");
+                        }}).validationErrors(new HashMap<String,String>(){{
+                            put("matchRegexp","必须以/开头");
+                        }}).placeholder("请输入菜单路由")
+                            .render(),
+                AmisFactory.NumberControl().name("order").label("排序")
+                        .required(true)
+                        .displayMode("enhance")
+                        .description("升序")
+                        .min(0)
+                        .value(0)
+                        .render(),
+                AmisFactory.ListControl().name("urlType").label("路由类型")
+                        .options(new HashMap<String,Object>(){{
+                            put("1","路由");
+                            put("2","外链");
+                        }}).value(1).render(),
+                AmisFactory.SwitchControl().name("visible").label("是否可见")
+                        .onText("显示")
+                        .offText("隐藏")
+                        .value(1)
+                        .render(),
+                AmisFactory.SwitchControl().name("isHome").label("是否为首页")
+                        .onText("是")
+                        .offText("否")
+                        .value(0)
+                        .description("是否为首页")
+                        .render(),
+                AmisFactory.SwitchControl().name("isFull").label("是否全屏")
+                        .onText("是")
+                        .offText("否")
+                        .value(0)
+                        .description("是否全屏")
+                        .render(),
+
+
+
+//                    amis()->TextControl('url', __('admin.admin_menu.url'))
+//                ->required()
+//                ->validateOnChange()
+//                ->validations(['matchRegexp' => '/^(http(s)?\:\/)?(\/)+/'])
+//                ->validationErrors(['matchRegexp' => __('admin.need_start_with_slash')])
+//                ->placeholder('eg: /admin_menus'),
+//                amis()->NumberControl('order', __('admin.admin_menu.order'))
+//                ->required()
+//                ->displayMode('enhance')
+//                ->description(__('admin.order_asc'))
+//                ->min(0)
+//                ->value(0),
+//                amis()->ListControl('url_type', __('admin.admin_menu.type'))
+//                ->options(Admin::adminMenuModel()::getType())
+//                ->value(Admin::adminMenuModel()::TYPE_ROUTE),
+//        amis()->SwitchControl('visible', __('admin.admin_menu.visible'))
+//                ->onText(__('admin.admin_menu.show'))
+//                ->offText(__('admin.admin_menu.hide'))
+//                ->value(1),
+//                amis()->SwitchControl('is_home', __('admin.admin_menu.is_home'))
+//                ->onText(__('admin.yes'))
+//                ->offText(__('admin.no'))
+//                ->description(__('admin.admin_menu.is_home_description'))
+//                ->value(0),
+//                amis()->SwitchControl('is_full', __('admin.admin_menu.is_full'))
+//                ->onText(__('admin.yes'))
+//                ->offText(__('admin.no'))
+//                ->description(__('admin.admin_menu.is_full_description'))
+//                ->value(0),
+//                AmisFactory.TextControl().name("parentId").label("parentId").render(),
+//                AmisFactory.TextControl().name("title").label("菜单名称").render(),
+//                AmisFactory.TextControl().name("icon").label("菜单图标").render(),
+//                AmisFactory.TextControl().name("url").label("菜单路由").render(),
+//                AmisFactory.TextControl().name("order").label("排序").render(),
+//
+//                AmisFactory.TextControl().name("urlType").label("路由类型(1:路由,2:外链)").render(),
+//                AmisFactory.TextControl().name("visible").label("是否可见").render(),
+//                AmisFactory.TextControl().name("isHome").label("是否为首页").render(),
+//                AmisFactory.TextControl().name("extension").label("扩展").render(),
             }
         ).render();
 
